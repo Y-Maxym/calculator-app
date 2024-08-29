15b8b6a3d9aba99cf4e748111ad87f197db5ba38
Maxym <maxym0198@gmail.com>
2024-08-29 21:55:05 +0300

    made adjustments

--- src/main/java/com/calculator/app/Calculator.java
+++ src/main/java/com/calculator/app/Calculator.java
@@ -1,23 +1,24 @@
-package calculator.app;
+package com.calculator.app;
 
 public class Calculator {
 
-    public Integer addition(Integer a, Integer b) {
+    public int addition(int a, int b) {
         return a + b;
     }
 
-    public Integer subtraction(Integer a, Integer b) {
+    public int subtraction(int a, int b) {
         return a - b;
     }
 
-    public Integer multiplication(Integer a, Integer b) {
+    public int multiplication(int a, int b) {
         return a * b;
     }
 
-    public Double division(Integer a, Integer b) {
+    public double division(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Division by zero");
         }
+
         return 1.0 * a / b;
     }
 }
