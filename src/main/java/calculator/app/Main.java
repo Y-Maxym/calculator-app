287e5f3669c381f47c9a211bf01f9d46a98ae02b
Maxym <maxym0198@gmail.com>
2024-08-29 13:31:42 +0300

    add main class with logic

--- src/main/java/calculator/app/Main.java
+++ src/main/java/calculator/app/Main.java
@@ -1,4 +1,21 @@
 package calculator.app;
 
 public class Main {
+
+    public Integer addition(Integer a, Integer b) {
+        return a + b;
+    }
+
+    public Integer subtraction(Integer a, Integer b) {
+        return a - b;
+    }
+
+    public Integer multiplication(Integer a, Integer b) {
+        return a * b;
+    }
+
+    public Integer division(Integer a, Integer b) {
+        if (b == 0) throw new ArithmeticException("Division by zero");
+        return a / b;
+    }
 }
